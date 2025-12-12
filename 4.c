e5a60384b5d3bd000a65229cebbdc14005948475
Sam Guard <sam.guard@hpe.com>
2025-12-12 16:14:13 +0000

    Day 4 done

--- 4.c
+++ 4.c
@@ -10,7 +10,7 @@
 #define FALSE 0
 #define TRUE !FALSE
 
-int get_adj(char grid[static MAX_GRID_SIZE], int width, int height, int pos) {
+int get_adj(const char grid[static MAX_GRID_SIZE], int width, int height, int pos) {
     int num_adj = 0;
     for (int dx = -1; dx <= 1; dx++) {
         for (int dy = -1; dy <= 1; dy++) {
@@ -28,25 +28,51 @@ int get_adj(char grid[static MAX_GRID_SIZE], int width, int height, int pos) {
     return num_adj;
 }
 
-uint64_t run_through(char grid[static MAX_GRID_SIZE], int width, int height) {
+uint64_t run_through(const char grid[static MAX_GRID_SIZE], int width, int height) {
     uint64_t score = 0;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             int pos = x + y * width;
             if (grid[pos] == '@' && get_adj(grid, width, height, pos) < 4) {
                 score++;
-                printf("%d %d\n", x, y);
             }
         }
     }
     return score;
 }
 
+uint64_t run_through2(const char grid_in[static MAX_GRID_SIZE], int width, int height) {
+    char grid[MAX_GRID_SIZE];
+    uint64_t score = 0;
+    int is_same;
+
+    memcpy(grid, grid_in, sizeof (grid));
+
+    do {
+        is_same = TRUE;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pos = x + y * width;
+                if (grid[pos] == '@' && get_adj(grid, width, height, pos) < 4) {
+                    score++;
+                    grid[pos] = '.';
+                    is_same = FALSE;
+                }
+                printf("%c", grid[pos]);
+            }
+            printf("\n");
+        }
+    } while (is_same == FALSE);
+
+    return score;
+}
+
+
 int main(int argc, char* argv[]) {
     char fname[256];
     char *in_buff;
     FILE *f;
-    uint64_t score = 0; // , score2 = 0;
+    uint64_t score = 0, score2 = 0;
     char grid[MAX_GRID_SIZE] = {0};
     int width = -1, row = 0;
 
@@ -79,8 +105,10 @@ int main(int argc, char* argv[]) {
     }
 
     score = run_through(grid, width, row);
+    score2 = run_through2(grid, width, row);
 
-    printf("Score -> %lu\n", score);
+    printf("Score   -> %lu\n", score);
+    printf("Score 2 -> %lu\n", score2);
 
     fclose(f);
     free(in_buff);
